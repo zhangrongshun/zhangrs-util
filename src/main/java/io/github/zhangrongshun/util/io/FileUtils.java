7784223274e94ce395695fb553af6935965d510a
zhangrs <zhangrs@yeah.net>
2024-01-14 12:23:44 +0800

    1

--- src/main/java/io/github/zhangrongshun/util/io/FileUtils.java
+++ src/main/java/io/github/zhangrongshun/util/io/FileUtils.java
@@ -1,14 +1,12 @@
 package io.github.zhangrongshun.util.io;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.LineIterator;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 public class FileUtils {
@@ -24,27 +22,6 @@ public class FileUtils {
         } catch (IOException ex) {
             IOUtils.closeQuietly(inputStream, ex::addSuppressed);
             throw ex;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        try (CustomizedLineIterator customizedLineIterator = FileUtils.customizedLineIterator(new File("D:\\log\\1.dat"), StandardCharsets.UTF_8, "\n")) {
-            int i = 1;
-            while (customizedLineIterator.hasNext()) {
-                String next = customizedLineIterator.next();
-                System.out.println("第" + i + "行, " + next);
-                i++;
-            }
-            System.out.println(i);
-        }
-        try (LineIterator customizedLineIterator = org.apache.commons.io.FileUtils.lineIterator(new File("D:\\log\\1.dat"), StandardCharsets.UTF_8.name())) {
-            int i = 1;
-            while (customizedLineIterator.hasNext()) {
-                String next = customizedLineIterator.next();
-                System.out.println("第" + i + "行, " + next);
-                i++;
-            }
-            System.out.println(i);
         }
     }
 
