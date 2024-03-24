d40da950470fcebd6f825714e31e61afca851339
zhangrs <zhangrs@yeah.net>
2024-03-24 20:30:30 +0800

    1

--- src/main/java/io/github/zhangrongshun/util/io/ExtFiles.java
+++ src/main/java/io/github/zhangrongshun/util/io/ExtFiles.java
@@ -4,7 +4,10 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UncheckedIOException;
 import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,9 +23,17 @@ public class ExtFiles {
     }
 
     public static Stream<String> lines(Path path, Charset cs, String lineSeparator) throws IOException {
-        CustomLineSeparatorBufferedReader br = new CustomLineSeparatorBufferedReader(new InputStreamReader(Files.newInputStream(path), cs), lineSeparator);
+        CharsetDecoder decoder = cs.newDecoder();
+        Reader reader = new InputStreamReader(Files.newInputStream(path), decoder);
+        CustomLineSeparatorBufferedReader br = new CustomLineSeparatorBufferedReader(reader, lineSeparator);
         try {
-            return br.lines().onClose(() -> IOUtils.closeQuietly(br));
+            return br.lines().onClose(() -> {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            });
         } catch (Error | RuntimeException e) {
             IOUtils.close(br, e::addSuppressed);
             throw e;
