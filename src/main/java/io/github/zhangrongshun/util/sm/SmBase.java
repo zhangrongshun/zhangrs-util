3e759522f392c772f4eb56ae5a5de4b9861f2b2c
zhangrs <zhangrs@yeah.net>
2024-04-17 22:26:06 +0800

    1

--- src/main/java/io/github/zhangrongshun/util/sm/SmBase.java
+++ src/main/java/io/github/zhangrongshun/util/sm/SmBase.java
@@ -1,13 +1,27 @@
 package io.github.zhangrongshun.util.sm;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.security.Provider;
 import java.security.Security;
 
 public class SmBase {
 
+    private static final Logger logger = LoggerFactory.getLogger(SmBase.class);
+
     static {
-        Security.addProvider(new BouncyCastleProvider());
+        final String name = "BC";
+        Provider bc = Security.getProvider(name);
+        if (bc == null) {
+            Security.addProvider(new BouncyCastleProvider());
+            Provider bc1 = Security.getProvider(name);
+            if (bc1 == null) {
+                throw new SecurityException("BouncyCastle provider not available");
+            }
+        }
+        logger.info("BouncyCastle provider available: {}", bc);
     }
 
 }
