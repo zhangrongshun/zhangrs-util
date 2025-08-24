b6add4849892cdd4236074e1fa8bf38ee2d8daea
zhangrs <zhangrs@yeah.net>
2025-08-24 09:03:28 +0800

    1

--- src/main/java/io/github/zhangrongshun/util/sm/RateLimiterWrapper.java
+++ src/main/java/io/github/zhangrongshun/util/sm/RateLimiterWrapper.java
@@ -11,14 +11,17 @@ public class RateLimiterWrapper {
     private final RateLimiter rateLimiter;
     private final AtomicDouble permitsPerSecond;
 
-    public RateLimiterWrapper(double permitsPerSecond) {
-        this.rateLimiter = RateLimiter.create(permitsPerSecond);
-        this.permitsPerSecond = new AtomicDouble(permitsPerSecond);
+    private RateLimiterWrapper(Builder builder) {
+        this.permitsPerSecond = new AtomicDouble(builder.permitsPerSecond);
+        if (builder.warmupPeriod != null) {
+            this.rateLimiter = RateLimiter.create(builder.permitsPerSecond, builder.warmupPeriod);
+        } else {
+            this.rateLimiter = RateLimiter.create(builder.permitsPerSecond);
+        }
     }
 
-    public RateLimiterWrapper(double permitsPerSecond, Duration warmupPeriod) {
-        this.rateLimiter = RateLimiter.create(permitsPerSecond, warmupPeriod);
-        this.permitsPerSecond = new AtomicDouble(permitsPerSecond);
+    public static Builder builder() {
+        return new Builder();
     }
 
     public void updateRate(double permitsPerSecond) {
@@ -50,4 +53,28 @@ public class RateLimiterWrapper {
         return rateLimiter.tryAcquire(timeout);
     }
 
+    public static class Builder {
+        private double permitsPerSecond;
+        private Duration warmupPeriod;
+
+        private Builder() {
+        }
+
+        public Builder permitsPerSecond(double permitsPerSecond) {
+            this.permitsPerSecond = permitsPerSecond;
+            return this;
+        }
+
+        public Builder warmupPeriod(Duration warmupPeriod) {
+            this.warmupPeriod = warmupPeriod;
+            return this;
+        }
+
+        public RateLimiterWrapper build() {
+            if (permitsPerSecond <= 0) {
+                throw new IllegalArgumentException("permitsPerSecond must be positive");
+            }
+            return new RateLimiterWrapper(this);
+        }
+    }
 }
